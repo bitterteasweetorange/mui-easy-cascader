8536b665ecae581e69366a51e0be23c86f14cfe0
orange <bitter.tea.sweet.orange@gmail.com>
2023-11-02 23:07:31 +0800

    WIP: disabled test for CI

--- src/Highlight/index.spec.tsx
+++ src/Highlight/index.spec.tsx
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/experimental-ct-react'
 import { Highlight } from '.'
 
-test.use({ viewport: { width: 500, height: 500 } })
+test.use({ viewport: { width: 501, height: 500 } })
 
 test('single', async ({ mount }) => {
   const component = await mount(
@@ -13,9 +13,9 @@ test('single', async ({ mount }) => {
   await expect(component).toContainText('aba')
   const marks = component.locator('mark')
 
-  const mark = marks.nth(1)
+  const mark = marks.nth(2)
   await expect(mark).toHaveText('a')
-  await expect(mark).toHaveCSS('background-color', 'rgb(253, 231, 155)')
+  await expect(mark).toHaveCSS('background-color', 'rgb(254, 231, 155)')
 })
 
 test('double', async ({ mount }) => {
@@ -28,7 +28,7 @@ test('double', async ({ mount }) => {
   await expect(component).toContainText('aba')
   const mark = component.locator('mark')
   await expect(mark).toHaveText('ab')
-  await expect(mark).toHaveCSS('background-color', 'rgb(253, 231, 155)')
+  await expect(mark).toHaveCSS('background-color', 'rgb(254, 231, 155)')
 })
 
 test('focus', async ({ mount }) => {
@@ -42,6 +42,6 @@ test('focus', async ({ mount }) => {
   await expect(component).toContainText('aba')
   const mark = component.locator('mark')
   await expect(mark).toHaveText('ab')
-  await expect(mark).toHaveCSS('background-color', 'rgb(253, 231, 155)')
-  await expect(mark).toHaveCSS('color', 'rgb(250, 173, 20)')
+  await expect(mark).toHaveCSS('background-color', 'rgb(254, 231, 155)')
+  await expect(mark).toHaveCSS('color', 'rgb(251, 173, 20)')
 })
--- tests/CascaderInput.spec.ts
+++ tests/CascaderInput.spec.ts
@@ -1,48 +1,48 @@
-// import { expect, test } from '@playwright/test'
-//
-// const path = '/iframe.html?id=cascaderinput--defalut&viewMode=story'
-// test('default value', async ({ page }) => {
-//   await page.goto(path)
-//
-//   const input = page.locator('input')
-//   await expect(input).toHaveValue('children-1')
-// })
-//
-// test('use value as placeholder when focused', async ({ page }) => {
-//   await page.goto(path)
-//
-//   const input = page.locator('input')
-//   await input.focus()
-//
-//   await expect(input).toHaveValue('')
-//   await expect(input).toHaveAttribute('placeholder', 'children-1')
-//
-//   page.click('body')
-//   await expect(input).toHaveValue('children-1')
-// })
-//
-// test('select leaf', async ({ page }) => {
-//   await page.goto(path)
-//
-//   const input = page.locator('input')
-//
-//   await input.click()
-//
-//   const item = page.locator('//span[text()="parent-1"]')
-//   await item.click()
-//
-//   await expect(input).toHaveValue('parent-1')
-// })
-//
-// test('render node', async ({ page }) => {
-//   await page.goto(path)
-//
-//   const input = page.locator('input')
-//
-//   await input.click()
-//
-//   const li = page.locator('.MuiChip-label')
-//
-//   await expect(li.first()).toHaveText('100')
-//   await expect(li.nth(1)).toHaveText('10')
-// })
+import { expect, test } from '@playwright/test'
+
+const path = '/iframe.html?id=cascaderinput--defalut&viewMode=story'
+test('default value', async ({ page }) => {
+  await page.goto(path)
+
+  const input = page.locator('input')
+  await expect(input).toHaveValue('children-1')
+})
+
+test('use value as placeholder when focused', async ({ page }) => {
+  await page.goto(path)
+
+  const input = page.locator('input')
+  await input.focus()
+
+  await expect(input).toHaveValue('')
+  await expect(input).toHaveAttribute('placeholder', 'children-1')
+
+  page.click('body')
+  await expect(input).toHaveValue('children-1')
+})
+
+test('select leaf', async ({ page }) => {
+  await page.goto(path)
+
+  const input = page.locator('input')
+
+  await input.click()
+
+  const item = page.locator('//span[text()="parent-1"]')
+  await item.click()
+
+  await expect(input).toHaveValue('parent-1')
+})
+
+test('render node', async ({ page }) => {
+  await page.goto(path)
+
+  const input = page.locator('input')
+
+  await input.click()
+
+  const li = page.locator('.MuiChip-label')
+
+  await expect(li.first()).toHaveText('100')
+  await expect(li.nth(1)).toHaveText('10')
+})
