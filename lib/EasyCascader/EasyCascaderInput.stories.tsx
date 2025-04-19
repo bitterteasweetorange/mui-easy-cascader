4c1fd4aef84f239858f66d8e88079bc741359933
orange <bitter.tea.sweet.orange@gmail.com>
2025-04-19 21:45:24 +0800

    fix: export component tree

--- lib/EasyCascader/EasyCascaderInput.stories.tsx
+++ lib/EasyCascader/EasyCascaderInput.stories.tsx
@@ -13,6 +13,7 @@ const meta = {
     (StoryBase) => (
       <Box
         sx={{
+          display: 'grid',
           width: 400,
           height: 200,
         }}
--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,4 @@
 export * from './component'
 export * from './EasyCascader'
+export * from './EasyTree'
 export * from './utils'
